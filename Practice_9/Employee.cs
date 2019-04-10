b8e7ff96e44ea27d7cf2a3527df032e3b6fc808a
Rustam Galeev <Vincent-cat@mail.ru>
2019-04-11 03:20:35 +0600

    Добавьте файлы проекта.

---
+++ Practice_9/Employee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_9
+{
+    public struct Employee
+    {
+        public string Name { get; set; }
+        public Vacancies Vacancy { get; set; }
+        public int Salary { get; set; }
+        public int[] DateOfBegin { get; set; }
+
+        public Employee(string name, Vacancies vacancy, int salary, int[] date)
+        {
+            Name = name;
+            Vacancy = vacancy;
+            Salary = salary;
+            DateOfBegin = date;
+        }
+        public void Show()
+        {
+            Console.Write($"Name: {Name}" +
+                $"\nVacancy: {Vacancy}" +
+                $"\nSalary: {Salary}" +
+                $"\nDateOfEngagement: ");
+            foreach (var i in DateOfBegin)
+            {
+                Console.Write(i + ".");
+            }
+        }
+    }
---
+++ Practice_9/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_9
+{
+    public enum Vacancies
+    {
+        Boss,
+        Manager,
+        Accountant,
+        Clerk,
+        Salesman
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
